c2e7e82be8212b308dd21ad85f11009afc9e4a39
lyz <li_yizhe@qq.com>
2015-08-11 20:35:20 +0800

    Initial Commit

---
+++ ColorfullRect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorfullRect
+//
+//  Created by 李懿哲 on 8/11/15.
+//  Copyright (c) 2015 lyzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorfullRect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorfullRect
+//
+//  Created by 李懿哲 on 8/11/15.
+//  Copyright (c) 2015 lyzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
